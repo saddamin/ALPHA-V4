fc71996584a88f97fdc112f2dcdea0e2d730ed99
saddamin <104092669+saddamin@users.noreply.github.com>
2022-04-22 19:32:10 +0530

    Update TrexTeam.js

--- TrexTeam.js
+++ TrexTeam.js
@@ -1,5 +1,5 @@
 module.exports = {
-    ALPHA_OWNER: '94784506970', // Trex Team - Alpha Owner
-    TREX_TEAM: '94771039631,94784506970,94716184907,94711421243,94771251788', // Team - Trex TEAM
-    TREX_OWNER: '94771039631' // Hirush Rukshan - Trex Owner --> For The Friendship And co Owner in Alpha 
+    ALPHA_OWNER: '94704421161', // Trex Team - Alpha Owner
+    TREX_TEAM: '94704421161,94779062397,94704247900,94711421243,94771251788', // Team - Trex TEAM
+    TREX_OWNER: '94704421161' // Sadeesha Min - Trex Owner --> For The Friendship And co Owner in Alpha 
 };
